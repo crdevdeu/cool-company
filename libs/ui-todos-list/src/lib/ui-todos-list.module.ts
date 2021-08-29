2adfa86d0715b4c2637371891fb508004eb4985b
Carlos Rincones <carlostomasrincones@gmail.com>
2021-08-29 13:27:08 +0200

    feat(ui-todo-list): add mock list

--- libs/ui-todos-list/src/lib/ui-todos-list.module.ts
+++ libs/ui-todos-list/src/lib/ui-todos-list.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiTodoListComponent } from './ui-todo-list.component';
+import { NbListModule, NbCardModule } from '@nebular/theme';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NbListModule, NbCardModule],
   declarations: [UiTodoListComponent],
   exports: [UiTodoListComponent],
 })
