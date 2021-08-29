1ec5c6305b3cf500aad8d94f40a5d95dd4b33508
Carlos Rincones <carlostomasrincones@gmail.com>
2021-08-29 14:49:27 +0200

    feat(ui): add markup elements

--- libs/ui-create-todo/src/lib/ui-create-todo.module.ts
+++ libs/ui-create-todo/src/lib/ui-create-todo.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiCreateTodoComponent } from './ui-create-todo.component';
+import {
+  NbFormFieldModule,
+  NbCardModule,
+  NbIconModule,
+  NbButtonModule,
+  NbInputModule,
+} from '@nebular/theme';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NbFormFieldModule,
+    NbCardModule,
+    NbIconModule,
+    NbButtonModule,
+    NbInputModule,
+  ],
   declarations: [UiCreateTodoComponent],
   exports: [UiCreateTodoComponent],
 })
--- libs/ui-todos-list/src/lib/ui-todos-list.module.ts
+++ libs/ui-todos-list/src/lib/ui-todos-list.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiTodoListComponent } from './ui-todo-list.component';
-import { NbListModule, NbCardModule } from '@nebular/theme';
+import {
+  NbListModule,
+  NbCardModule,
+  NbIconModule,
+  NbButtonModule,
+} from '@nebular/theme';
 
 @NgModule({
-  imports: [CommonModule, NbListModule, NbCardModule],
+  imports: [
+    CommonModule,
+    NbListModule,
+    NbCardModule,
+    NbIconModule,
+    NbButtonModule,
+  ],
   declarations: [UiTodoListComponent],
   exports: [UiTodoListComponent],
 })
