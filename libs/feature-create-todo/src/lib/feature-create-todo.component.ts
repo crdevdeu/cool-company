3059a45bc3a0c26a5d1d485a5855a1dfdd9d6f1b
Carlos Rincones <carlostomasrincones@gmail.com>
2021-08-30 23:32:03 +0200

    feat(create todo ui): add  toast notification when creating a todo

--- libs/feature-create-todo/src/lib/feature-create-todo.component.ts
+++ libs/feature-create-todo/src/lib/feature-create-todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TodosFacade } from '@cool-company/shared-todos-state';
+import { NbToastrService } from '@nebular/theme';
 
 @Component({
   selector: 'cool-company-feature-create-todo',
@@ -7,11 +8,23 @@ import { TodosFacade } from '@cool-company/shared-todos-state';
   styleUrls: ['./feature-create-todo.component.css'],
 })
 export class FeatureCreateTodoComponent implements OnInit {
-  constructor(private todosFacade: TodosFacade) {}
+  constructor(
+    private todosFacade: TodosFacade,
+    private toastrService: NbToastrService
+  ) {}
 
   ngOnInit(): void {}
 
   onSaveTodo(todo: any) {
     this.todosFacade.createTodo(todo);
+    this.showToast('top-right', 'primary');
+  }
+
+  showToast(position: any, status: any) {
+    this.toastrService.show('Todo created', `Success`, {
+      position,
+      status,
+      icon: '',
+    });
   }
 }
--- libs/feature-create-todo/src/lib/feature-create-todo.module.ts
+++ libs/feature-create-todo/src/lib/feature-create-todo.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatureCreateTodoComponent } from './feature-create-todo.component';
 import { UiCreateTodoModule } from '@cool-company/ui-create-todo';
+import { NbToastrModule, NbToastrService } from '@nebular/theme';
 
 @NgModule({
-  imports: [CommonModule, UiCreateTodoModule],
+  imports: [CommonModule, UiCreateTodoModule, NbToastrModule.forRoot({})],
   declarations: [FeatureCreateTodoComponent],
   exports: [FeatureCreateTodoComponent],
+  providers: [NbToastrService],
 })
 export class FeatureCreateTodoModule {}
